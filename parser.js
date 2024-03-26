7513cd2771079dda60d879734bdc69eb381d9f52
Ahmed Sadman <ahmedsadman.211@gmail.com>
2024-03-27 04:45:54 +0600

    send email when operator is found

--- parser.js
+++ parser.js
@@ -9,7 +9,7 @@ class Parser {
         this.page = null;
     }
 
-    ensurePage() {
+    _ensurePage() {
         if (!this.page) {
             throw new Error('Page not found. Make sure you parsed the page by calling .parse');
         }
@@ -25,18 +25,20 @@ class Parser {
     }
 
     async noResultsFound() {
-        this.ensurePage();
+        this._ensurePage();
         const noResultsNode = await this.page.$('.bold_ticket_text');
         return Boolean(noResultsNode);
     }
 
     async getBusOperatorNames() {
+        this._ensurePage();
         const elements = await this.page.$$('.op_name');
         const operators = await Promise.all(elements.map(el => el.evaluate(e => e.textContent)));
         return operators;
     }
 
     async hasBusOperator(operator) {
+        this._ensurePage();
         const operators = await this.getBusOperatorNames();
         return operators.findIndex(op => op === operator) >= 0;
     }
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import yargs from 'yargs';
 import Parser from './parser.js';
+import mailer from './mailer.js';
 
 (async () => {
     const argv = yargs(process.argv.slice(2))
@@ -25,12 +26,16 @@ import Parser from './parser.js';
             return;
         }
     
-        const operators = await parser.getBusOperatorNames();
-        console.log(operators);
-        console.log(await parser.hasBusOperator(argv.operator));
+        if (await parser.hasBusOperator(argv.operator)) {
+            const subject = `Bus ticket for ${argv.operator} is available`;
+            const text = 'This is a reminder. Check out the website to book tickets'
+            const messageId = await mailer.send(argv.email, subject, text);
+            console.log(`Email Sent: ${messageId}`);
+        }
     } catch (err) {
         console.log(err);
     } finally {
         parser.close();
+        console.log('Check complete');
     }
 })();
